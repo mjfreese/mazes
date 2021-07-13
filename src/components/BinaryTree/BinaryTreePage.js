7ee3f43dcd0f720584f3da295926b098180cc4c6
Matt Freese <matthew.freese@iptglobal.com>
2021-07-13 15:56:42 -0500

    Add Binary Tree maze generation

--- src/components/BinaryTree/BinaryTreePage.js
+++ src/components/BinaryTree/BinaryTreePage.js
@@ -1,8 +1,35 @@
 import React from 'react'
 import GridLayout from "../Layout/GridLayout"
 
+const generateModel = (cols, rows) => {
+    const model = []
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            if (i === cols - 1 && j === 0)
+                model.push({x: i, y: j, borderWidth: '1px 1px 0 0'})
+            else if (i === cols - 1 && j === rows - 1)
+                model.push({x: i, y: j, borderWidth: '0 1px 1px 0'})
+            else if (i === cols - 1)
+                model.push({x: i, y: j, borderWidth: '0 1px 0 0'})
+            else if (j === 0)
+                model.push({x: i, y: j, borderWidth: `1px 0 0 ${i === 0 ? 1 : 0}px`})
+            else {
+                const rand = Math.floor(Math.random() * 2)
+                model.push({x: i, y: j, borderWidth: `${rand}px ${rand === 0 ? 1 : 0}px ${j === rows - 1 ? 1 : 0}px ${i === 0 ? 1 : 0}px`, openSide: rand})
+            }
+        }
+    }
+
+    console.log(model)
+    return model
+}
+
 const BinaryTreePage = () => {
-    return (<div><GridLayout/></div>)
+    const square = 12
+    const model = generateModel(square, square)
+
+    return (<div><GridLayout model={model} cols={square}/></div>)
 }
 
 export default BinaryTreePage
--- src/components/Layout/GridLayout.js
+++ src/components/Layout/GridLayout.js
@@ -1,7 +1,38 @@
 import React from 'react'
+import RGL, { WidthProvider } from 'react-grid-layout'
 
-const GridLayout = () => {
-    return (<div>Grid Layout</div>)
+const ReactGridLayout = WidthProvider(RGL)
+
+const defaultModel = [{x: 0, y: 0, borderWidth: '1px'}, {x: 1, y: 0, borderWidth: '1px'}, {x: 12, y: 0, borderWidth: '1px'}]
+
+const generateLayout = (model) => {
+    return model.map((cell, index) => ({x: cell.x, y: cell.y, w: 1, h: 1, i: `${index}`}))
+}
+
+const generateDom = (model) => {
+    return model.map((cell, index) => (
+        <div key={index} style={{borderStyle: 'solid', borderColor: 'black', borderWidth: cell.borderWidth, textAlign:'center'}}>
+        </div>
+    ))
+}
+
+const onLayoutChange = (layout) => {
+    
+}
+
+const GridLayout = ({model, cols = 12}) => {
+    return (
+        <ReactGridLayout
+            layout={generateLayout(model ?? defaultModel)}
+            onLayoutChange={onLayoutChange}
+            className='layout'
+            isDraggable={false}
+            isResizable={false}
+            cols={cols}
+        >
+        {generateDom(model ?? defaultModel)}
+        </ReactGridLayout>
+    )
 }
 
 export default GridLayout
