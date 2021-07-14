6af186127ed053534f5d12ec75866c636fde14cc
Matt Freese <matthew.freese@iptglobal.com>
2021-07-14 09:03:19 -0500

    switch to hash router, fix homepage

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {HashRouter, Route, Switch} from 'react-router-dom'
 
 import BinaryTreePage from '../components/BinaryTree/BinaryTreePage'
 import HomePage from '../components/Home/HomePage'
 
 const AppRoutes = () => (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <Switch>
             <Route path='/' exact component={HomePage}/>
             <Route path='/BinaryTree' exact component={BinaryTreePage}/>
         </Switch>
-    </BrowserRouter>
+    </HashRouter>
 )
 
 export default AppRoutes
