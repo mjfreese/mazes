ccf3b0a6806b16721c747b0c0a8be36b3059083d
Matt Freese <matthew.freese@iptglobal.com>
2021-07-14 09:07:03 -0500

    try 404 redirect trick with BrowserRouter

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import {HashRouter, Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import BinaryTreePage from '../components/BinaryTree/BinaryTreePage'
 import HomePage from '../components/Home/HomePage'
 
 const AppRoutes = () => (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Switch>
             <Route path='/' exact component={HomePage}/>
             <Route path='/BinaryTree' exact component={BinaryTreePage}/>
         </Switch>
-    </HashRouter>
+    </BrowserRouter>
 )
 
 export default AppRoutes
