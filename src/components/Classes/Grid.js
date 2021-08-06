9da1007e597ff87c13adeb091b32784c4f6e64e5
Matthew Freese <matthew.freese@iptglobal.com>
2021-08-06 10:58:12 -0500

    finish grid implementation

--- src/components/Classes/Grid.js
+++ src/components/Classes/Grid.js
@@ -19,7 +19,34 @@ export class Grid {
         return rows
     }
 
-    prepareCells() {
+    prepareCells(cells) {
+        cells.forEach((cell) => {
+            const row = cell.row
+            const col = cell.col
 
+            cell.north = this.getCell(row - 1, col)
+            cell.south = this.getCell(row + 1, col)
+            cell.east = this.getCell(row, col + 1)
+            cell.west = this.getCell(row, col - 1)
+        })
+    }
+
+    getCell(row, col) {
+        if (row >= this.rows || row < 0)
+            return null
+        if (col < 0 || col >= this.grid[row].length)
+            return null
+        
+        return this.grid[row][col]
+    }
+
+    getRandomCell() {
+        const row = Math.floor(Math.random() * this.rows)
+        const col = Math.floor(Math.random() * this.grid[row].length)
+        return this.getCell(row, col)
+    }
+
+    getSize() {
+        return this.rows * this.columns
     }
 }
